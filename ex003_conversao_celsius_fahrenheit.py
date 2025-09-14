5ba8f47e082492ca7ac8f59c5f2a6b532dba33f0
Rafael Mistura <rafaelfernandesm12345@gmail.com>
2025-09-14 17:19:59 -0300

    Delete ex003_conversao_celsius_fahrenheit.py

--- ex003_conversao_celsius_fahrenheit.py
+++
@@ -1,4 +0,0 @@
-print('Obs: Digite apenas números!!!!!!!!!')
-temp = int(input('Digite a temperatura em Celsius: '))
-resultado = (temp * 9/5) + 32
-print('A temperatura em Fahrenheit é:', resultado)
