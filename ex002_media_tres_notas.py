59f0d1ba5823a2779f98e071c77aee13822bef72
Rafael Mistura <rafaelfernandesm12345@gmail.com>
2025-09-14 17:20:13 -0300

    Delete ex002_media_tres_notas.py

--- ex002_media_tres_notas.py
+++
@@ -1,5 +0,0 @@
-numero1 = int(input('Nota em matematica: '))
-numero2 = int(input('Nota em portugues: '))
-numero3 = int(input('Nota em quimica: '))
-resultado = (numero1 + numero2 + numero3)/2
-print('A média da suas notas são:', resultado)
